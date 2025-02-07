6b1d9ffdcac6bd5fe299a6a2a5710abbbad3e5f7
Christopher Raquet <xf3778@kit.edu>
2025-02-07 10:07:54 +0100

    Remove sparql search button

--- src/components/search/DefaultSearchBox.tsx
+++ src/components/search/DefaultSearchBox.tsx
@@ -1,8 +1,7 @@
 import type { InputViewProps } from "@elastic/react-search-ui-views"
 import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
-import { ChevronDown, ExternalLink, SearchIcon } from "lucide-react"
+import { SearchIcon } from "lucide-react"
 
 export function DefaultSearchBox(props: InputViewProps) {
     return (
@@ -10,23 +9,9 @@ export function DefaultSearchBox(props: InputViewProps) {
             <SearchIcon className="rfs-absolute rfs-left-7 md:rfs-left-12 rfs-top-3.5 rfs-size-4 rfs-text-muted-foreground md:rfs-top-auto" />
             <Input {...props.getInputProps()} placeholder="Search for something..." className="rfs-h-11 rfs-pl-10" />
             <div className="rfs-flex rfs-w-full rfs-items-center md:rfs-h-full md:rfs-w-auto">
-                <Button {...props.getButtonProps()} className="rfs-grow rfs-rounded-r-none" size="lg">
+                <Button {...props.getButtonProps()} variant="secondary" className="rfs-grow" size="lg">
                     Search
                 </Button>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button size="icon" className="rfs-h-full rfs-rounded-l-none rfs-border-l rfs-border-l-background">
-                            <ChevronDown className="rfs-size-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <a href="https://metarepo.nffa.eu/start_query" target="_blank">
-                            <DropdownMenuItem>
-                                <ExternalLink className="rfs-size-4" /> Go to SparQL search
-                            </DropdownMenuItem>
-                        </a>
-                    </DropdownMenuContent>
-                </DropdownMenu>
             </div>
         </div>
     )
