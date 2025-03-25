5d172acfffcf8f50f63ad0d05e240d535a34a939
Christopher Raquet <xf3778@kit.edu>
2025-03-25 10:37:49 +0100

    Update dependencies and fix demo

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/**/*.mdx"],
     addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-themes", "@storybook/addon-docs"],
     framework: {
         name: "@storybook/react-vite",
