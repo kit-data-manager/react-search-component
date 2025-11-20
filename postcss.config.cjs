a3b3d25c19fb2113762be27d12628ee81421723e
Christopher Raquet <xf3778@kit.edu>
2025-11-20 11:29:37 +0100

    fix: scope css rules and bump version

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+const PostCSSPrefixWrap = require("postcss-prefixwrap")
+
+/**
+ * Wrap the entire generate tailwind css file (index.css) in .rfs-root to prevent style conflicts with other tailwind styles
+ * This would otherwise be problematic when embedding this component in a page that also uses tailwind with a different theme
+ */
+
+module.exports = {
+    plugins: [PostCSSPrefixWrap(".rfs-root")]
+}
